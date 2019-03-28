50cbabadead048dba0b1b5202c507f3be9280a42
dydelf <mgajewskik@gmail.com>
2019-03-28 22:31:55 +0700

    regions update

--- project_population/data_processing.py
+++ project_population/data_processing.py
@@ -16,11 +16,11 @@ with open(filename) as f:
     pop_data = json.load(f)
 
 
-#for pop_dict in pop_data:
-#    if pop_dict['Year'] == '2010':
-#        country_name = pop_dict['Country Name']
-#        population = pop_dict['Value']
-#        print(country_name + ' ' + population)
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name']
+        population = pop_dict['Value']
+        print(country_name + ' ' + population)
 
 
 # data sets for plotting
@@ -28,17 +28,20 @@ arab_pop = {}
 euro_pop = {}
 na_pop = {}
 se_pop = {}
+la_pop = {}
+a_pop = {}
+
 w_pop = {}
 
 
 #extracting the data
 for pop_dict in pop_data:
-    if pop_dict['Country Name'] == 'Arab World':
+    if pop_dict['Country Name'] == 'Middle East & North Africa (all income levels)':
         arab_year = int(float(pop_dict['Year']))
         arab_population = int(float(pop_dict['Value']))
         arab_pop[arab_year] = arab_population
 for pop_dict in pop_data:
-    if pop_dict['Country Name'] == 'European Union':
+    if pop_dict['Country Name'] == 'Europe & Central Asia (all income levels)':
         euro_year = int(float(pop_dict['Year']))
         euro_population = int(float(pop_dict['Value']))
         euro_pop[euro_year] = euro_population
@@ -48,15 +51,26 @@ for pop_dict in pop_data:
         na_population = int(float(pop_dict['Value']))
         na_pop[na_year] = na_population
 for pop_dict in pop_data:
-    if pop_dict['Country Name'] == 'South Asia':
+    if pop_dict['Country Name'] == 'East Asia & Pacific (all income levels)':
         se_year = int(float(pop_dict['Year']))
         se_population = int(float(pop_dict['Value']))
         se_pop[se_year] = se_population
+for pop_dict in pop_data:
+    if pop_dict['Country Name'] == 'Latin America & Caribbean (all income levels)':
+        la_year = int(float(pop_dict['Year']))
+        la_population = int(float(pop_dict['Value']))
+        la_pop[la_year] = la_population
+for pop_dict in pop_data:
+    if pop_dict['Country Name'] == 'Sub-Saharan Africa (all income levels)':
+        a_year = int(float(pop_dict['Year']))
+        a_population = int(float(pop_dict['Value']))
+        a_pop[a_year] = a_population
 for pop_dict in pop_data:
     if pop_dict['Country Name'] == 'World':
         w_year = int(float(pop_dict['Year']))
         w_population = int(float(pop_dict['Value']))
         w_pop[w_year] = w_population
+
 
 
 visual = pygal.Line()
@@ -65,10 +79,12 @@ visual.title = "Population during last years in regions of the World"
 visual.x_title = "Year"
 visual.y_title = "Population"
 visual.x_labels = [i for i in range(1960, 2010)]
-visual.add('Arab Countries', arab_pop)
-visual.add('Euro Countries', euro_pop)
+visual.add('Middle East & North Africa', arab_pop)
+visual.add('Europe & Central Asia', euro_pop)
 visual.add('North America', na_pop)
-visual.add('South East Asia', se_pop)
+visual.add('East Asia & Pacific', se_pop)
+visual.add('Latin America & Caribbean', la_pop)
+visual.add('Sub-Saharan Africa', a_pop)
 
 visual.render_to_file('population_visual.svg')
 

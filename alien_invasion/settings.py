8182e5221a354c725aa694a350cc2199c519e5de
dydelf <mgajewskik@gmail.com>
2019-03-29 21:36:35 +0700

    alien invasion added

---
+++ alien_invasion/settings.py
@@ -0,0 +1,25 @@
+"""
+File containing settings for the game Alien Invasion.
+"""
+
+
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game settings."""
+
+        # Screen settings
+        self.screen_width = 1024
+        self.screen_height = 540
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3

69e1110de02b41f6a642b6c2e2091bc67fc23152
dydelf <mgajewskik@gmail.com>
2019-05-14 20:44:05 +0200

    polished version

--- interactive_dictionary/dictionary.py
+++ interactive_dictionary/dictionary.py
@@ -8,16 +8,29 @@ from difflib import get_close_matches
 data = json.load(open("data.json", "r"))
 
 def translate(word):
-    word = word.lower()
+    """ Searching for a word in database, if not found matching the closest
+    possible word from the database."""
     if word in data:
         return data[word]
-    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
-        print("\nDid you mean %s instead?" % get_close_matches(word,
-            data.keys(), cutoff=0.8)[0])
-        word = get_close_matches(word, data.keys(), cutoff=0.8)[0]
-        return data[word]
-    else:
-        return "The word doesn't exist. Please double check it."
+    elif word not in data:
+        word = word.upper()
+        if word in data:
+            return data[word]
+        else:
+            word = word.lower()
+            if word in data:
+                return data[word]
+            elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+                choice = input("\nDid you mean %s instead? | y/n | "
+                        % get_close_matches(word, data.keys(), cutoff=0.8)[0])
+                if choice == "n":
+                    word = get_close_matches(word, data.keys(), cutoff=0.8)[1]
+                    return data[word]
+                else:
+                    word = get_close_matches(word, data.keys(), cutoff=0.8)[0]
+                    return data[word]
+            else:
+                return "The word doesn't exist. Please double check it."
 
 active = True
 while active:

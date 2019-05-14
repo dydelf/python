593300559db46bf9a23de4de9679347906af076c
dydelf <mgajewskik@gmail.com>
2019-05-14 20:25:51 +0200

    polishing touches

--- interactive_dictionary/dictionary.py
+++ interactive_dictionary/dictionary.py
@@ -10,22 +10,26 @@ data = json.load(open("data.json", "r"))
 def translate(word):
     word = word.lower()
     if word in data:
-        for i in data[word]:
-            print(i)
+        return data[word]
     elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
         print("\nDid you mean %s instead?" % get_close_matches(word,
             data.keys(), cutoff=0.8)[0])
         word = get_close_matches(word, data.keys(), cutoff=0.8)[0]
-        for i in data[word]:
-            print(i)
+        return data[word]
     else:
         return "The word doesn't exist. Please double check it."
 
 active = True
 while active:
+    print("Get the definition of the word.")
     word = input("Enter word: ")
+    output = translate(word)
     print("\n")
-    print(translate(word))
+    if type(output) == str:
+        print(output)
+    else:
+        for i in output:
+            print(i)
     flag = input("\nWould you like another word? | y/n | ")
     if flag == "n":
         active = False

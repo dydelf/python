22fbb7c750168b5a984709295037cc4d5b3e9898
dydelf <mgajewskik@gmail.com>
2019-06-12 21:02:52 +0200

    changes in virtual env, new project start

---
+++ projects/learning_log/learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Defines url patterns for learning logs."""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+        #Home page
+        url(r'^$', views.index, name='index'),
+    ]

d8b06aec9661fede6e0bb50869829b68584aa6f2
dydelf <mgajewskik@gmail.com>
2019-06-16 12:52:01 +0200

    further work on a project

---
+++ projects/free_write/data.py
@@ -0,0 +1,30 @@
+"""
+Simulated file of a MySql database.
+"""
+
+
+def articles():
+    article = [
+        {
+            'id': 1,
+            'title': 'Log One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Unknown',
+            'create_date': '15-06-2019',
+        },
+        {
+            'id': 2,
+            'title': 'Log Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Unknown',
+            'create_date': '16-06-2019',
+        },
+        {
+            'id': 3,
+            'title': 'Log Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'author': 'Unknown',
+            'create_date': '17-06-2019',
+        },
+    ]
+    return article
